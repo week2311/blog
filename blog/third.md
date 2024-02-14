b261cae52edea5aa9ed1c5e65000b1e7592b01f1
root <root@localhost.localdomain>
2024-02-14 15:39:53 +0800

    first commit

---
+++ blog/third.md
@@ -0,0 +1,12 @@
+---
+slug: third-blog
+title: third
+date: 2023-04-12 14:09:58
+tags: [third]
+authors: RiverMountain
+---
+
+
+## 介绍
+
+这是使用dasdnladnkankasdhajidhajsb
---
+++ sidebars.js
@@ -0,0 +1,20 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  stack: [
+    'stack/introduction',
+    'stack/test',
+    {
+      // label: 'Docusaurus',
+      // type: 'category',
+      // link: {
+      //   type: 'doc',
+      //   id: 'blog/Docusaurus/Docusaurus-guides'
+      // },
+      // items: [
+
+      // ],
+    },
+  ],
+}
+
+module.exports = sidebars
